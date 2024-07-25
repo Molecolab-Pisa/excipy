d0e9cc4f18a58906ea16cd2bb0b7ac81f785e31a
Edoardo Cignoni <edoardo.cignoni96@gmail.com>
2024-07-25 12:28:25 +0200

    added authors + license in each file

--- excipy/regression.py
+++ excipy/regression.py
@@ -1,3 +1,18 @@
+# excipy: machine learning models for a fast estimation of excitonic Hamiltonians
+# Copyright (C) 2022 excipy authors
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <https://www.gnu.org/licenses/>.
 from __future__ import annotations
 
 from collections.abc import Iterable
